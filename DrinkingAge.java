4f3b172d973f1ab3636e4440c91e6efa235e7e8d
Levi Minch <levi.minch@gmail.com>
2019-10-03 09:27:00 -0700

    First Commit.

---
+++ DrinkingAge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DrinkingAge{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your age?");
+    int age = scan.nextInt();
+    if (age >= 21)
+      System.out.println("You are old enough to drink");
+    else
+      System.out.println("You are not old enought to drink");
+
+  }
+}
