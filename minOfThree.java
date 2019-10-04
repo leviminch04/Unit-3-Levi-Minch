f3b788050eec0f3ab00a243eeaf1d700a244f970
Levi Minch <levi.minch@gmail.com>
2019-10-04 08:52:42 -0700

    minofthree edit
    minofthree edit

--- minOfThree.java
+++ minOfThree.java
@@ -12,8 +12,8 @@ public class minOfThree {
     if (num1 <= num2 && num1 <= num3)
       System.out.println("Your smallest number was: " + num1);
     else if (num2 <= num1 && num2 <= num3)
-      System.out.println("Your biggest number was: " + num2);
+      System.out.println("Your smallest number was: " + num2);
     else if (num3 <= num1 && num3 <= num2)
-      System.out.println("Your biggest number was: " + num3);
+      System.out.println("Your smallest number was: " + num3);
   }
 }
