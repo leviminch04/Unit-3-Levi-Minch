56d9b945ef37f724ad6b6b562009dbc1c8b83da3
Levi Minch <levi.minch@gmail.com>
2019-10-10 08:56:01 -0700

    rounds to 10th instead of 100th

--- winPercentage.java
+++ winPercentage.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 public class winPercentage {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    DecimalFormat fmt = new DecimalFormat("0.##");
+    DecimalFormat fmt = new DecimalFormat("0.#");
     boolean running = true;
     boolean calculate = true;
     while (running){
