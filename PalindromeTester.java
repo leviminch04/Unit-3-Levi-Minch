7eb83cc32ce72591e18ed990a517ef31bfb16f24
Levi Minch <levi@Levis-MacBook-Pro.local>
2019-10-10 20:02:10 -0700

    update palindrometester
    bye bye error

--- PalindromeTester.java
+++ PalindromeTester.java
@@ -11,32 +11,33 @@ public class PalindromeTester {
       String yes = "y";
       String no = "n";
       run = false;
+      running = true;
+      while(running){
+        while (yn.equals(yes) && running == true){
+          System.out.println("What is your palindrome?");
+          String palindrome = scan.next();
+          String palindromeLower = palindrome.toLowerCase();
+          char[] palindromArray = palindromeLower.toCharArray();
+          String reversed = new StringBuilder(new String(palindromArray)).reverse().toString();
+          if (reversed.equals(palindrome)){
+            System.out.println("Your string: " + palindrome + " is a palindrome");
+            running = false;
+            run = true;
+          }
+          if (reversed.equals(palindrome) == false){
+            System.out.println("Your string: " + palindrome + " is not a palindrome");
+            running = false;
+            run = true;
+          }
+
+        }
+        if (yn.equals(no)){
+          System.out.println("Ok Then");
+          running = false;
+        }
+      }
     }
 
-    while(running){
-      while (yn.equals(yes) && running == true){
-        System.out.println("What is your string?");
-        String palindrome = scan.next();
-        String palindromeLower = palindrome.toLowerCase();
-        char[] palindromArray = palindromeLower.toCharArray();
-        String reversed = new StringBuilder(new String(palindromArray)).reverse().toString();
-        if (reversed.equals(palindrome)){
-          System.out.println("Your string: " + palindrome + " is a palindrome");
-          running = false;
-          run = true;
-        }
-        if (reversed.equals(palindrome) == false){
-          System.out.println("Your string: " + palindrome + " is not a palindrome");
-          running = false;
-          run = true;
-        }
-
-      }
-      if (yn.equals(no)){
-        System.out.println("Ok Then");
-        running = false;
-      }
-    }
 
 
   }
