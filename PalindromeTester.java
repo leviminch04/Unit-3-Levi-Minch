6c1e0a4592cfdc4f4d9adf0a963ccfe797e7475a
Levi Minch <levi@Levis-MacBook-Pro.local>
2019-10-10 19:46:06 -0700

    update pilondrum
    error with: while(run)

--- PalindromeTester.java
+++ PalindromeTester.java
@@ -3,20 +3,41 @@ import java.util.Scanner;
 public class PalindromeTester {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    System.out.println("Would you like to enter a palindrome (y/n)");
-    String yn = scan.next();
-    String yes = "y";
-    String no = "n";
+    boolean run = true;
     boolean running = true;
-    while (yn.equals(yes)){
-      System.out.println("What is your string?");
-      String palindrome = scan.next();
-      String palindromeLower = palindrome.toLowerCase();
+    while(run){
+      System.out.println("Would you like to enter a palindrome (y/n)");
+      String yn = scan.next();
+      String yes = "y";
+      String no = "n";
+      run = false;
     }
-    if (yn.equals(no)){
-      System.out.println("Ok Then");
 
+    while(running){
+      while (yn.equals(yes) && running == true){
+        System.out.println("What is your string?");
+        String palindrome = scan.next();
+        String palindromeLower = palindrome.toLowerCase();
+        char[] palindromArray = palindromeLower.toCharArray();
+        String reversed = new StringBuilder(new String(palindromArray)).reverse().toString();
+        if (reversed.equals(palindrome)){
+          System.out.println("Your string: " + palindrome + " is a palindrome");
+          running = false;
+          run = true;
+        }
+        if (reversed.equals(palindrome) == false){
+          System.out.println("Your string: " + palindrome + " is not a palindrome");
+          running = false;
+          run = true;
+        }
+
+      }
+      if (yn.equals(no)){
+        System.out.println("Ok Then");
+        running = false;
+      }
     }
+
 
   }
 }
